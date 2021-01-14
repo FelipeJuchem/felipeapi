25a52ea6556deecb4a6a79c08dbc09101fa4baa0
FelipeJuchem <felipe_build@hotmail.com>
2021-01-13 21:45:22 -0300

    h2

--- src/main/java/com/felipe/api/entities/User.java
+++ src/main/java/com/felipe/api/entities/User.java
@@ -1,9 +1,17 @@
 package com.felipe.api.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
+import java.util.UUID;
 
+@Entity
 public class User  {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
