4b2c07a6311f622e2542e60940cc46c2229d2dab
FelipeJuchem <felipe_build@hotmail.com>
2021-01-16 11:44:54 -0300

    order

--- src/main/java/com/felipe/api/entities/Order.java
+++ src/main/java/com/felipe/api/entities/Order.java
@@ -1,6 +1,7 @@
 package com.felipe.api.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.felipe.api.entities.enums.OrderStatus;
 
 import javax.persistence.*;
 import java.time.Instant;
@@ -17,6 +18,8 @@ public class Order {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -24,9 +27,10 @@ public class Order {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -52,6 +56,15 @@ public class Order {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() { return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
