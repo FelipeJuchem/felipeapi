2b5d3f2c9e367a2e1d409bcdb4d39a33c7a2cb33
FelipeJuchem <felipe_build@hotmail.com>
2021-01-17 09:21:27 -0300

    Product

---
+++ src/main/java/com/felipe/api/services/ProductService.java
@@ -0,0 +1,31 @@
+package com.felipe.api.services;
+
+import com.felipe.api.entities.Product;
+import com.felipe.api.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll(){
+        return repository.findAll();
+    }
+
+    public Product findById(Long id){
+        Optional<Product> obj = repository.findById(id);
+        return obj.get();
+    }
+
+
+
+}
+
+
+
